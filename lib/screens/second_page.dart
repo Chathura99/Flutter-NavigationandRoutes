8ed123c15fd7fb74ed8afb4542c8990caac74744
Chathura <chathura.madura11@gmail.com>
2022-06-30 14:56:46 +0530

    ListView

--- lib/screens/second_page.dart
+++ lib/screens/second_page.dart
@@ -3,30 +3,65 @@ import 'package:stu_management/screens/home_page.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         title: const Text('Second Page'),
-         leading: IconButton(
+        title: const Text('Second Page'),
+        leading: IconButton(
           icon: const Icon(Icons.home),
-          onPressed:(){
-            Navigator.pop(context,const MyHomePage(title: ""));
-          } ,
-          ),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Second Page',
-            ),
-          ],
+          onPressed: () {
+            Navigator.pop(context, const MyHomePage(title: ""));
+          },
         ),
+      ),
+      body: ListView(
+        // scrollDirection:Axis.vertical,
+        children: [
+          Text("Second Page"),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text("Camera"),
+            subtitle: Text("settings"),
+            trailing: Icon(Icons.menu) ,
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text("Camera"),
+            subtitle: Text("settings"),
+            trailing: Icon(Icons.menu) ,
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text("Camera"),
+            subtitle: Text("settings"),
+            trailing: Icon(Icons.menu) ,
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text("Camera"),
+            subtitle: Text("settings"),
+            trailing: Icon(Icons.menu) ,
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text("Camera"),
+            subtitle: Text("settings"),
+            trailing: Icon(Icons.menu) ,
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text("Camera"),
+            subtitle: Text("settings"),
+            trailing: Icon(Icons.menu) ,
+            onTap: (){},
+          ),
+          ],
       ),
     );
   }
